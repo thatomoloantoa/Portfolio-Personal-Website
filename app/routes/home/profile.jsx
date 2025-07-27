62f0448ab8204598e7c79c6006636d4695a40b12
ec883596t2@standardbank.onmicrosoft.com <71700841+Tahfentse@users.noreply.github.com>
2025-07-27 22:40:13 +0200

    first commit

---
+++ app/routes/home/profile.jsx
@@ -0,0 +1,102 @@
+import profileImgLarge from '~/assets/Thato_3.jpg';
+import profileImgPlaceholder from '~/assets/Thato_2.jpg';
+import profileImg from '~/assets/profile.jpg';
+import { Button } from '~/components/button';
+import { DecoderText } from '~/components/decoder-text';
+import { Divider } from '~/components/divider';
+import { Heading } from '~/components/heading';
+import { Image } from '~/components/image';
+import { Link } from '~/components/link';
+import { Section } from '~/components/section';
+import { Text } from '~/components/text';
+import { Transition } from '~/components/transition';
+import { Fragment, useState } from 'react';
+import { media } from '~/utils/style';
+import katakana from './katakana.svg';
+import styles from './profile.module.css';
+
+const ProfileText = ({ visible, titleId }) => (
+  <Fragment>
+    <Heading className={styles.title} data-visible={visible} level={3} id={titleId}>
+      <DecoderText text="Hi there" start={visible} delay={500} />
+    </Heading>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      I’m Thato, currently I live in Johhanesburg,braamfountein doing the graduate program at{' '} 
+      <Link href="https://uvuafrica.com/capaciti/">Capaciti</Link>. I am a dedicated and detail-orientated
+      Persona with a solid foundation in IT systems,project coordinantion and business analysis.
+      Proficient in leveraging technical knowledge to optimize processe and deliver innovative solutions.
+      Adapt at collaborating with cross-functional teams,enhancing system efficieny, and providing exceptional-
+      support .
+    </Text>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      In my spare time I like to play chess, watch movies and{' '}
+        also watch movies . I’m always down for hearing
+      about new projects, so feel free to drop me a line.
+    </Text>
+  </Fragment>
+);
+
+export const Profile = ({ id, visible, sectionRef }) => {
+  const [focused, setFocused] = useState(false);
+  const titleId = `${id}-title`;
+
+  return (
+    <Section
+      className={styles.profile}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      as="section"
+      id={id}
+      ref={sectionRef}
+      aria-labelledby={titleId}
+      tabIndex={-1}
+    >
+      <Transition in={visible || focused} timeout={0}>
+        {({ visible, nodeRef }) => (
+          <div className={styles.content} ref={nodeRef}>
+            <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="/contact"
+                icon="send"
+              >
+                Send me a message
+              </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+                  About me
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={`${profileImg} 480w, ${profileImgLarge} 960w`}
+                  width={960}
+                  height={1280}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt="Me in my Graduation."
+                />
+                <svg className={styles.svg} data-visible={visible} viewBox="0 0 136 766">
+                  <use href={`${katakana}#katakana-profile`} />
+                </svg>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </Section>
+  );
+};
